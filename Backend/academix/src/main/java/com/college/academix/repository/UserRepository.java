cd032a07c33585779d6f285c1a3b119c96ae788e
Hariharan V S <iamhariharan.offcl@gmail.com>
2026-03-04 15:31:03 +0530

    Basic working backend

--- Backend/academix/src/main/java/com/college/academix/repository/UserRepository.java
+++ Backend/academix/src/main/java/com/college/academix/repository/UserRepository.java
@@ -1,5 +1,10 @@
 package com.college.academix.repository;
 
-public class UserRepository {
-    
+import com.college.academix.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User,Long>{
+
+    User findByEmail(String email);
+
 }
